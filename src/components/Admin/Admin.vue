30581d924c853b2003335198f4d38b8e6f869bd4
JHAIR DANIEL OSORIO GARZON <89557050+DOSORIO1@users.noreply.github.com>
2022-10-31 17:47:20 -0500

    da

--- src/components/Admin/Admin.vue
+++ src/components/Admin/Admin.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="container-main"> 
+  <div> 
     <router-view></router-view>
     </div>
 </template>
 
 
 <style scoped>
-.container-main {
+/* .container-main {
   padding: 0% !important;
   margin: 0% !important;
   overflow: hidden;
   width: 100vw;
   height: 100%;
  
-}
+} */
 
 </style>
 
--- src/components/Admin/Clients.vue
+++ src/components/Admin/Clients.vue
@@ -27,16 +27,16 @@
         <tbody>
           <tr v-for="c in clients" :key="'clients' + c.id">
             <td>{{ c.name }}</td>
-            <td>{{ c.age}}</td>
-            <td>{{ c.weight}}</td>
+            <td>{{ c.age }}</td>
+            <td>{{ c.weight }}</td>
             <td>{{ c.nivel }}</td>
             <td>{{ c.email }}</td>
             <td>{{ c.injures }}</td>
-            <td>{{ c.start_date}}</td>
-            <td>{{ c.finish_date}}</td>
-            <td>{{ c.rates}}</td>
-            <td>{{ c.price}}</td>
-            <td>{{ c.companies_id}}</td>
+            <td>{{ c.start_date }}</td>
+            <td>{{ c.finish_date }}</td>
+            <td>{{ c.rates }}</td>
+            <td>{{ c.price }}</td>
+            <td>{{ c.companies_id }}</td>
 
             <td>
               <button type="button" class="btn btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop"
@@ -72,81 +72,72 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+          <h5 class="modal-title" id="staticBackdropLabel" style="color:white; "> New client</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
 
 
           <form>
-            <div>
-              <label for="">name</label>
-              <input type="text" name="name" v-model="form.name" />
-            </div>
-            <br>
-
-            <div>
-              <label for="">age</label>
-              <input type="number" name="age" v-model="form.age" />
-            </div>
-            <br>
-            <div>
-              <label for="">weight</label>
-              <input type="number" name="weight" v-model="form.weight" />
-            </div>
-            <br>
-            <div>
-              <label for="">nivel</label>
-              <input type="text" name="nivel" v-model="form.nivel" />
-            </div>
-            <br>
-            <div>
-              <label for="">email</label>
-              <input type="email" name="email" v-model="form.email" />
-            </div>
-            <br>
-            <div>
-              <label for="">injures</label>
-              <input type="text" name="injures" v-model="form.injures" />
-            </div>
-            <br>
-            <div>
-              <label for="">injures</label>
-              <input type="text" name="injures" v-model="form.injures" />
-            </div>
-            <br>
-            <div>
-              <label for="">start date</label>
-              <input type="date" name="injures" v-model="form.start_date" />
-            </div>
-            <br>
-            <div>
-              <label for=""> finish date</label>
-              <input type="date" name="injures" v-model="form.finish_date" />
-            </div>
-            <br>
-            <div>
-              <label for=""> payment period</label>
-              <input type="text" name="injures" v-model="form.rates" />
-            </div>
-            <br>
-
-            <article>
-              <select class="form-select" aria-label="Default select example" aria-placeholder="seleciona tu periodo de pago"
-              name="rates"
-             >
-                
-                <option v-for=" t in rates_list" 
-                :value="t.id"
-                :key="'rates' + t.id"
-                :selected="true"><p>{{t.name}}</p><p>{{t.price}}</p></option>
-                
-              </select>
-            </article>
-           
+            <div id="izq">
+              <div class="form-floating mb-3">
+                <input type="text" name="name" v-model="form.name" class="form-control" id="floatingInput"
+                  placeholder="">
+                <label for="floatingInput">name</label>
+              </div>
+              <div class="form-floating mb-3">
+                <input type="email" name="email" v-model="form.email" class="form-control" id="floatingInput"
+                  placeholder="name@example.com">
+                <label for="floatingInput"> email</label>
+              </div>
+              <div class="form-floating mb-3">
+                <input type="password" name="password" v-model="form.password" class="form-control" id="floatingInput"
+                  placeholder="password">
+                <label for="floatingInput"> password</label>
+              </div>
+              <div class="form-floating mb-3">
+                <input type="password" name="password" v-model="form.password_confirmation" class="form-control"
+                  id="floatingInput" placeholder="password_confirmation">
+                <label for="floatingInput">password confirmation</label>
+              </div>
+              <div class="form-floating mb-3">
+                <input type="number" name="age" v-model="form.age" class="form-control" id="floatingInput"
+                  placeholder="">
+                <label for="floatingInput">age</label>
+              </div>
+              <div class="form-floating mb-3">
+                <input type="number" name="weight" v-model="form.weight" class="form-control" id="floatingInput"
+                  placeholder="">
+                <label for="floatingInput"> weight</label>
+              </div>
 
 
+            </div>
 
+
+            <div id="dere">
+              <div class="form-floating mb-3">
+                <input type="text" name="nivel" v-model="form.nivel" class="form-control" id="floatingInput"
+                  placeholder="">
+                <label for="floatingInput"> nivel</label>
+              </div>
+
+              <div class="form-floating mb-3">
+                <input type="text" name="injures" v-model="form.injures" class="form-control" id="floatingInput"
+                  placeholder="">
+                <label for="floatingInput"> injures</label>
+              </div>
+              <div class="form-floating mb-3">
+                <input type="date" name="injures" v-model="form.start_date" class="form-control" id="floatingInput"
+                  placeholder="">
+                <label for="floatingInput"> start date</label>
+              </div>
+              <div class="form-floating mb-3">
+                <input type="date" name="injures" v-model="form.finish_date" class="form-control" id="floatingInput"
+                  placeholder="">
+                <label for="floatingInput"> finish date</label>
+              </div>
+            </div>
 
           </form>
 
@@ -154,8 +145,38 @@
 
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary " @click="store()" data-bs-dismiss="modal"> created</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"   data-bs-toggle="modal" data-bs-target="#staticBackdrop3">select pago</button>
+          <!-- <button type="button" class="btn btn-primary " @click="store()" data-bs-dismiss="modal"> created</button> -->
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- Modal  rates -->
+  <div class="modal fade" id="staticBackdrop3" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <article id="article" v-for="t in rates_list" :key="'rates_list' + t.id">
+              <p>
+                {{t.name}}
+              </p>
+
+            </article>
+
+
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="updated()">edit</button>
+
         </div>
       </div>
     </div>
@@ -217,8 +238,8 @@
               <input type="text" name="injures" v-model="clients_edit.rates" />
             </div>
             <br>
-            
-              
+
+
             <br>
 
 
@@ -251,7 +272,7 @@ export default {
   data() {
     return {
       clients: [],
-      rates_list:[],
+      rates_list: [],
       clients_edit: {},
       form: {
         name: "",
@@ -263,7 +284,8 @@ export default {
         start_date: "",
         finish_date: "",
         rates: "",
-        
+        password: "",
+        password_confirmation: "",
         companies_id: ""
 
       },
@@ -319,17 +341,19 @@ export default {
       }
     },
 
-    async get_rates(){
+    async get_rates() {
       this.user = JSON.parse(localStorage.user);
       const companies_id = this.user.companies_id;
-      try{
+      try {
         const rs = await this.axios.post(`api/companies/rates/${companies_id}`);
         this.rates_list = rs.data.rates_list;
-        
-      }catch(e){
-console.log(e)
+        console.log(rates_list)
+
+      } catch (e) {
+        console.log(e)
       }
     },
+    
 
     edit_clients(c) {
       this.clients_edit = c;
